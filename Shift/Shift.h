6f37161ba6ba90ff49f47c5852e8be4cdb4ed345
Wesley Wickwire <wickwirew@gmail.com>
2019-11-11 18:20:06 -0500

    renamed scheme

---
+++ Shift/Shift.h
@@ -0,0 +1,19 @@
+//
+//  Shift.h
+//  Shift
+//
+//  Created by Wes Wickwire on 9/23/19.
+//  Copyright © 2019 Wes Wickwire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shift.
+FOUNDATION_EXPORT double ShiftVersionNumber;
+
+//! Project version string for Shift.
+FOUNDATION_EXPORT const unsigned char ShiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shift/PublicHeader.h>
+
+
